9b47ee4a31aa8c6bd79023adcdd4cb1b4950f61f
Dash <42308489+varnem2@users.noreply.github.com>
2018-11-03 18:35:01 -0400

    Updated RecipeBot

--- DiscordBotRecipes/index.js
+++ DiscordBotRecipes/index.js
@@ -4,18 +4,53 @@ const fs = require('fs');
 const url = 'https://discordapp.com/api/webhooks/497504768898629632/TmfjDaibdHZM0zMBqiNRBHPad3j80bYxlCuQYMVU0Wkk7Tf31PWundOR4syXqhjBiTHl?wait=true';
 const avatar_url = 'https://ih0.redbubble.net/image.436363942.7705/mp,550x550,matte,ffffff,t.3.jpg';
 const name = "Recipie Bot";
-var dir = 'C:/Users/dash/Documents/10SFG Recipies/';
+//var dir = 'C:/Users/dash/Documents/10SFG Recipies/';
+var dir = __dirname + '/Recipies/';
+const noPrep = 'Tomorrow\'s meal isn\'t prepped yet!';
+const prep = 'Tomorrow\'s meal is ';
+const headder = '--------------------------------------------------------';
 
-var j = schedule.scheduleJob('0 0 0 6 * *', function(){
-    console.log('The answer to life, the universe, and everything!');
-    var fileName = fs.readdirSync(dir)[0];
-    console.log(fileName);
+
+var onceADay = '0 0 0 6 * *';
+var every30Seconds = '30 * * * * *';
+var j = schedule.scheduleJob(every30Seconds, function(){
+    var fileNameArr = fs.readdirSync(dir);
+    var fileName = fileNameArr[0];
+
+    try{
+        request.post(url).form({
+            avatar_url: avatar_url, 
+            username: name, 
+            content: headder + "Get ready for tomorrow's recipe called " + fileNameArr[1].substring(0, fileNameArr[1].length - 4) + headder
+        });
+    }catch (e){
+        request.post(url).form({
+            avatar_url: avatar_url,
+            username: name,
+            content: headder + " Tomorrow's recipe isn't prepped yet " + headder,
+        });
+    }
+    
+    
     console.log();
-    var bufferOne = fs.readFileSync(dir + fileName).toString('utf8');
-    //console.log(bufferOne);
-    request.post(url).form({avatar_url: avatar_url, username: name, content: bufferOne});
+    console.log(fileName);
+    
+    request.post(url).form({
+        avatar_url: avatar_url, 
+        username: name,
+        content: fs.readFileSync(dir + fileName, function(err){
+            if(err){
+                console.log("Error on readFileSync");
+                throw err;
+            };
+        }).toString('utf8')
+    });
+
     fs.unlink(dir + fileName, function(err){
-        if(err) throw err;
+        if(err){
+            console.log("Error on unlink");
+            throw err;
+        }
         console.log('File deleted: ' + fileName);
     });
 });
