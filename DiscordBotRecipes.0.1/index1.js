ba271d1067923816db349af43761c2bc16dc9eac
Dash <42308489+varnem2@users.noreply.github.com>
2018-11-05 10:01:06 -0500

    I don't know what this is, but I'm pushing it so I can run it later.

--- DiscordBotRecipes.0.1/index1.js
+++ DiscordBotRecipes.0.1/index1.js
@@ -1,14 +1,55 @@
-/*var download = require('download-git-repo');
+//static init message
+const url = 'https://discordapp.com/api/webhooks/497504768898629632/TmfjDaibdHZM0zMBqiNRBHPad3j80bYxlCuQYMVU0Wkk7Tf31PWundOR4syXqhjBiTHl?wait=true';
+const avatar_url = 'https://ih0.redbubble.net/image.436363942.7705/mp,550x550,matte,ffffff,t.3.jpg';
+const name = "Recipie Bot";
+const noPrep = 'Tomorrow\'s meal isn\'t prepped yet!';
+const prep = 'Tomorrow\'s meal is ';
+const headder = '--------------------------------------------------------';
 
-download('varnem2/effective-pancake', 'Recipies', function(err){
-    console.log(err ? 'Error' : 'Success');
-});
-*/
+
+//everything else
+var request = require('request');
+const repo = 'https://github.com/varnem2/effective-pancake.git';
+var dir = __dirname + '/effective-pancake/';
 const fs = require('fs');
-const simpleGit = require('simple-git')()
-    .init()
-    .clone('https://github.com/varnem2/effective-pancake.git');
+var fileName = fs.readdirSync(dir);
 
 
+let pullGitHub = new Promise((resolve, reject) => {
+    const simpleGit = require('simple-git')().init().clone(repo);
+});
 
+let makePostToday = new Promise((resolve, reject) => {
+    
+    try{
+        request.post(url).form({
+            avatar_url: avatar_url,
+            username: name,
+            content: 
+                headder + 
+                prep + 
+                fileName[0].substring(
+                        0, 
+                        fileName[0].length - 4) + headder
+        })
+    }catch(e){
+        request.post(url).form({
+            avatar_url: avatar_url,
+            username: name,
+            content:
+                headder + noPrep + headder
+        })
+    }
+    
+    
 
+});
+
+let deleteLocalFile = new Promise((resolve, reject) => {
+    
+})
+
+let makePostTomorrow = new Promise((resolve, reject) => {
+    var fileName = fs.readdirSync(dir)[0];
+    
+})
