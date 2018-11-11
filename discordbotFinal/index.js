a05cd35eb204d9f24d34147f1c6bf7d8b1d7cfb8
Dash <42308489+varnem2@users.noreply.github.com>
2018-11-11 14:36:01 -0500

    updated time

--- discordbotFinal/index.js
+++ discordbotFinal/index.js
@@ -17,7 +17,7 @@ var fourpm = new schedule.RecurrenceRule();
 fourpm.hour = 7;
 fourpm.minute = 0;
 fourpm.second = 0;
-var j = schedule.scheduleJob(every10Seconds, function(){
+var j = schedule.scheduleJob(fourpm, function(){
     var fileNameArr = fs.readdirSync(dir);
     var fileName = fileNameArr[1];
 
